3042475b2e663d9973b848abaf3bdd534d02249a
Jami Laamanen <jami.a.laamanen@student.jyu.fi>
2019-07-10 07:52:21 +0000

    Exercise 1 done

--- get-phantomjs.sh
+++ get-phantomjs.sh
@@ -8,7 +8,7 @@ if [ ! -e "$HOME/phantomjs" ]; then
   echo "One moment, downloading PhantomJS..."
   TEMPFILE=$(mktemp)
   TEMPDIR=$(mktemp -d)
-  curl "http://phantomjs.googlecode.com/files/$PHANTOMJS.tar.bz2" > "$TEMPFILE"
+  curl "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2" > "$TEMPFILE"
   echo "Unpacking..."
   tar -C "$TEMPDIR" -xjf "$TEMPFILE"
   mv "$TEMPDIR/$PHANTOMJS/bin/phantomjs" "$HOME"
