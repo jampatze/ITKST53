0dd686065cda975dfa052fb5b34b68f4f3248449
Jami Laamanen <jami.a.laamanen@student.jyu.fi>
2019-07-10 11:42:30 +0000

    Exercise 2 done

--- get-phantomjs.sh
+++ get-phantomjs.sh
@@ -3,12 +3,12 @@
 # Download and install phantomjs; the version on Debian doesn't work with the
 # grading scripts, since it's missing phantom.clearCookies(), has some kind of
 # broken require(), and other fun things.
-PHANTOMJS=phantomjs-1.9.2-linux-i686
+PHANTOMJS=phantomjs-2.1.1-linux-i686
 if [ ! -e "$HOME/phantomjs" ]; then
   echo "One moment, downloading PhantomJS..."
   TEMPFILE=$(mktemp)
   TEMPDIR=$(mktemp -d)
-  curl "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2" > "$TEMPFILE"
+  wget "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2" > "$TEMPFILE"
   echo "Unpacking..."
   tar -C "$TEMPDIR" -xjf "$TEMPFILE"
   mv "$TEMPDIR/$PHANTOMJS/bin/phantomjs" "$HOME"
